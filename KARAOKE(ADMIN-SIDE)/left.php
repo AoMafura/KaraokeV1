2e3beb7f217dee510dc669d446601a6a70daf727
AoMafura <midori.mizune@gmail.com>
2021-01-20 00:09:54 +0800

    First Admin Commit

---
+++ KARAOKE(ADMIN-SIDE)/left.php
@@ -0,0 +1,10 @@
+<div class = "row">
+            <div class = "col-md-3 left">
+                <a href="NewCard.php" class="btn btn-primary my-2">Transaction History</a>
+				<a href="KaraokeHour.php" class="btn btn-primary my-2">Cards</a>
+                <a href="TransactionsHistory.php" class="btn btn-primary my-2">Employees</a>
+                <a href="KaraokeHour.php" class="btn btn-primary my-2">Song List</a>
+                <a href="TransactionsHistory.php" class="btn btn-primary my-2">Branches</a>
+
+                <a href = "Home.php" class="btn btn-primary logout text-white">Logout</a> 
+            </div>
